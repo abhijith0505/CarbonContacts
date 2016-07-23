f87c5ce3f07f88c83b67767036b44417630c68cd
Abhijith <abhijith4m0505@gmail.com>
2016-07-23 20:13:06 +0530

    Feature: View recently deleted contacts

--- app/src/main/java/abhijith/carboncontacts/RecentDeletes.java
+++ app/src/main/java/abhijith/carboncontacts/RecentDeletes.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -15,14 +20,16 @@ import java.util.ArrayList;
 public class RecentDeletes extends AppCompatActivity {
 
     ArrayList<PhoneContact> deletedContacts = new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recent_deletes);
-       // getActionBar().setDisplayHomeAsUpEnabled(true);
 
+        listView = (ListView) findViewById(R.id.list);
 
+        ArrayList<String> deleted = new ArrayList<>();
 
         SharedPreferences sharedpreferences = getSharedPreferences("deleted", Context.MODE_PRIVATE);
         int size = sharedpreferences.getInt("size", 0);
@@ -35,9 +42,43 @@ public class RecentDeletes extends AppCompatActivity {
 
         for(PhoneContact contact: deletedContacts){
             Log.d("delLOG: ", contact.getContactName() + ": " + contact.getContactNumber());
+            deleted.add(contact.getContactName() + "(" + contact.getContactType() + "): " + contact.getContactNumber());
         }
 
+        ArrayAdapter arrayAdapter = new ArrayAdapter(RecentDeletes.this, R.layout.row, deleted);
+        listView.setAdapter(arrayAdapter);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recent_deleted, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_delete_recent:
+                Toast.makeText(RecentDeletes.this, "History cleared", Toast.LENGTH_SHORT).show();
+                deleteSharedPref();
+                Intent intent = new Intent(RecentDeletes.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deleteSharedPref() {
+        SharedPreferences sharedpreferences = getSharedPreferences("deleted", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.clear();
+        editor.commit();
+    }
 
 }
